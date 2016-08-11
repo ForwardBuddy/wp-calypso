ForwardBuddy/wp-calypso

commit c982f4b60b51d1c89caac802d23753a79fe3f1c8

Site Settings: Further fixing of ESLint warnings in general site settings

client/my-sites/site-settings/form-general.jsx
@@ -195,9 +195,9 @@ const FormGeneral = React.createClass( {
 		}
 
 		return (
-			<FormFieldset className="has-divider">
+			<FormFieldset className="site-settings__has-divider">
 				<FormLabel htmlFor="blogaddress">{ this.translate( 'Site Address' ) }</FormLabel>
-				<div className="blogaddress-settings">
+				<div className="site-settings__blogaddress-settings">
 					<FormInput
 						name="blogaddress"
 						type="text"
@@ -260,7 +260,7 @@ const FormGeneral = React.createClass( {
 
 				<FormLabel>
 					<FormRadio
-						name="blog_public"
+						name="blog_public"Ï
 						value="0"
 						checked={ 0 === parseInt( this.state.blog_public, 10 ) }
 						onChange={ this.handleRadio }

client/my-sites/site-settings/style.scss
@@ -84,7 +84,7 @@
 		padding-top: 20px;
 	}
 
-	.blogaddress-settings {
+	.site-settings__blogaddress-settings {
 		@include breakpoint( ">660px" ) {
 			display: flex;
 		}
@@ -275,7 +275,8 @@
 	margin-right: 8px;
 }
 
-.has-divider {
+// @TODO: Remove has-divider once all instances have been replaced with .site-settings__has-divider
+.has-divider, .site-settings__has-divider {
 	margin: 0 -24px;
 	padding: 24px 24px 16px 24px;
 	border: 1px lighten( $gray, 30% ) solid;

commit 33e533b92c2f1f14e7f461964fe16b243c9790ba

remove typo introduced in c982f4b60b51d1c89caac802d23753a79fe3f1c8 (#7401)

client/my-sites/site-settings/form-general.jsx
@@ -258,7 +258,7 @@ const FormGeneral = React.createClass( {
 
 				<FormLabel>
 					<FormRadio
-						name="blog_public"Ï
+						name="blog_public"
 						value="0"
 						checked={ 0 === parseInt( this.state.blog_public, 10 ) }
 						onChange={ this.handleRadio }