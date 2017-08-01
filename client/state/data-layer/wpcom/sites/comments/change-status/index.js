ForwardBuddy/wp-calypso

commit d2fc57e723b52bd5128edd21f82994494572d524

Comments: Move changeCommentStatus into the data layer (#16678)
Move the `changeCommentStatus` implementation to the data layer, and replaces the `wpcom` API handler with the `http` middleware.
Remove `COMMENTS_CHANGE_STATUS_SUCCESS` and `COMMENTS_CHANGE_STATUS_FAILURE`, and makes full use of the `dispatchRequest` handlers.

The comment status is changed immediately after dispatching the `COMMENTS_CHANGE_STATUS` action.
On success, the comment status will be update again with the value returned by the API call.
On failure, the comment status will be updated back to its original value and an error notice will be displayed, with a "Try Again" button.

client/state/data-layer/wpcom/sites/comments/change-status/index.js
@@ -1,0 +1,71 @@
+/**
+ * External dependencies
+ */
+import { translate } from 'i18n-calypso';
+import { get, noop, omit } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { COMMENTS_CHANGE_STATUS } from 'state/action-types';
+import { mergeHandlers } from 'state/action-watchers/utils';
+import { local } from 'state/data-layer/utils';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { errorNotice, removeNotice } from 'state/notices/actions';
+import { getSiteComment } from 'state/selectors';
+
+const changeCommentStatus = ( { dispatch, getState }, action ) => {
+	const { siteId, commentId, status } = action;
+	const previousStatus = get( getSiteComment( getState(), action.siteId, action.commentId ), 'status' );
+
+	dispatch(
+		http(
+			{
+				method: 'POST',
+				path: `/sites/${ siteId }/comments/${ commentId }`,
+				apiVersion: '1.1',
+				body: {
+					status,
+				},
+			},
+			{
+				...action,
+				previousStatus,
+			}
+		)
+	);
+};
+
+const announceFailure = ( { dispatch, getState }, action ) => {
+	const { commentId, status, previousStatus } = action;
+
+	dispatch( removeNotice( `comment-notice-${ action.commentId }` ) );
+
+	dispatch(
+		local( {
+			...omit( action, [ 'meta' ] ),
+			status: previousStatus,
+		} )
+	);
+
+	const errorMessage = {
+		approved: translate( "We couldn't approve this comment." ),
+		unapproved: translate( "We couldn't unapprove this comment." ),
+		spam: translate( "We couldn't mark this comment as spam." ),
+		trash: translate( "We couldn't move this comment to trash." ),
+	};
+	const defaultErrorMessage = translate( "We couldn't update this comment." );
+
+	dispatch( errorNotice( get( errorMessage, status, defaultErrorMessage ), {
+		button: translate( 'Try again' ),
+		id: `comment-notice-${ commentId }`,
+		onClick: () => dispatch( omit( action, [ 'meta' ] ) ),
+	} ) );
+};
+
+const changeStatusHandlers = {
+	[ COMMENTS_CHANGE_STATUS ]: [ dispatchRequest( changeCommentStatus, noop, announceFailure ) ],
+};
+
+export default mergeHandlers( changeStatusHandlers );

client/state/data-layer/wpcom/sites/comments/index.js
@@ -16,6 +16,7 @@ import {
 } from 'state/action-types';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import changeStatus from './change-status';
 import replies from './replies';
 import likes from './likes';
 import { errorNotice } from 'state/notices/actions';
@@ -125,4 +126,4 @@ const fetchHandler = {
 	],
 };
 
-export default mergeHandlers( fetchHandler, replies, likes );
+export default mergeHandlers( fetchHandler, replies, likes, changeStatus );

client/state/action-types.js
@@ -62,8 +62,6 @@ export const BILLING_TRANSACTIONS_REQUEST_FAILURE = 'BILLING_TRANSACTIONS_REQUES
 export const BILLING_TRANSACTIONS_REQUEST_SUCCESS = 'BILLING_TRANSACTIONS_REQUEST_SUCCESS';
 export const COMMENT_REQUEST = 'COMMENT_REQUEST';
 export const COMMENTS_CHANGE_STATUS = 'COMMENTS_CHANGE_STATUS';
-export const COMMENTS_CHANGE_STATUS_FAILURE = 'COMMENTS_CHANGE_STATUS_FAILURE';
-export const COMMENTS_CHANGE_STATUS_SUCESS = 'COMMENTS_CHANGE_STATUS_SUCESS';
 export const COMMENTS_COUNT_INCREMENT = 'COMMENTS_COUNT_INCREMENT';
 export const COMMENTS_COUNT_RECEIVE = 'COMMENTS_COUNT_RECEIVE';
 export const COMMENTS_DELETE = 'COMMENTS_DELETE';

client/state/comments/actions.js
@@ -5,8 +5,6 @@ import { isEnabled } from 'config';
 import wpcom from 'lib/wp';
 import {
 	COMMENTS_CHANGE_STATUS,
-	COMMENTS_CHANGE_STATUS_FAILURE,
-	COMMENTS_CHANGE_STATUS_SUCESS,
 	COMMENTS_DELETE,
 	COMMENTS_EDIT,
 	COMMENTS_EDIT_FAILURE,
@@ -135,39 +133,21 @@ export const unlikeComment = ( siteId, postId, commentId ) => ( {
 	commentId,
 } );
 
-export function changeCommentStatus( siteId, postId, commentId, status ) {
-	return dispatch => {
-		dispatch( {
-			type: COMMENTS_CHANGE_STATUS,
-			siteId,
-			postId,
-			commentId,
-			status,
-		} );
-
-		return wpcom
-			.site( siteId )
-			.comment( commentId )
-			.update( { status } )
-			.then( data =>
-				dispatch( {
-					type: COMMENTS_CHANGE_STATUS_SUCESS,
-					siteId,
-					postId,
-					commentId,
-					status: data.status,
-				} ),
-			)
-			.catch( () =>
-				dispatch( {
-					type: COMMENTS_CHANGE_STATUS_FAILURE,
-					siteId,
-					postId,
-					commentId,
-				} ),
-			);
-	};
-}
+/**
+ * Creates an action that changes a comment status.
+ * @param {Number} siteId Site identifier
+ * @param {Number} postId Post identifier
+ * @param {Number} commentId Comment identifier
+ * @param {Number} status New status
+ * @returns {Object} Action that changes a comment status
+ */
+export const changeCommentStatus = ( siteId, postId, commentId, status ) => ( {
+	type: COMMENTS_CHANGE_STATUS,
+	siteId,
+	postId,
+	commentId,
+	status,
+} );
 
 export function editComment( siteId, postId, commentId, content ) {
 	return dispatch => {

client/state/happychat/middleware.js
@@ -23,7 +23,7 @@ import {
 	HELP_CONTACT_FORM_SITE_SELECT,
 	ROUTE_SET,
 
-	COMMENTS_CHANGE_STATUS_SUCESS,
+	COMMENTS_CHANGE_STATUS,
 	EXPORT_COMPLETE,
 	EXPORT_FAILURE,
 	EXPORT_STARTED,
@@ -219,7 +219,7 @@ export const getEventMessageFromActionData = ( action ) => {
 	// Below we've stubbed in the actions we think we'll care about, so that we can
 	// start incrementally adding messages for them.
 	switch ( action.type ) {
-		case COMMENTS_CHANGE_STATUS_SUCESS:
+		case COMMENTS_CHANGE_STATUS:
 			return `Changed a comment's status to "${ action.status }"`;
 		case EXPORT_COMPLETE:
 			return 'Export completed';
@@ -332,7 +332,7 @@ export default function( connection = null ) {
 			case HAPPYCHAT_INITIALIZE:
 				connectIfRecentlyActive( connection, store );
 				break;
-        
+
 			case HELP_CONTACT_FORM_SITE_SELECT:
 				updateChatPreferences( connection, store, action.siteId );
 				break;

commit 80add860f44d03911d46f375fe3c54d9061ddb4a

Comments: On change status, remove the error notice before retrying (#16785)
Fix a small bug introduced in #16678.
If an error happens when changing a comment status, a successful retry doesn't remove the error notice.

client/state/data-layer/wpcom/sites/comments/change-status/index.js
@@ -19,6 +19,8 @@ const changeCommentStatus = ( { dispatch, getState }, action ) => {
 	const { siteId, commentId, status } = action;
 	const previousStatus = get( getSiteComment( getState(), action.siteId, action.commentId ), 'status' );
 
+	dispatch( removeNotice( `comment-notice-${ commentId }` ) );
+
 	dispatch(
 		http(
 			{
@@ -39,8 +41,6 @@ const changeCommentStatus = ( { dispatch, getState }, action ) => {
 
 const announceFailure = ( { dispatch, getState }, action ) => {
 	const { commentId, status, previousStatus } = action;
-
-	dispatch( removeNotice( `comment-notice-${ action.commentId }` ) );
 
 	dispatch(
 		local( {