ForwardBuddy/wp-calypso

commit 6a30f774ef21099d3d6ddbabd76b9b7b473f2118

Framework: Run 'imports' codemod on `client/` (#18359)
Plus subsequent fixes.

client/post-editor/editor-discussion/index.jsx
@@ -2,17 +2,17 @@
  * External dependencies
  */
 import { get, pick } from 'lodash';
-const PropTypes = require( 'prop-types' );
-const React = require( 'react' );
+import PropTypes from 'prop-types';
+import React from 'react';
 
 /**
  * Internal dependencies
  */
-const EditorFieldset = require( 'post-editor/editor-fieldset' ),
-	FormCheckbox = require( 'components/forms/form-checkbox' ),
-	PostActions = require( 'lib/posts/actions' ),
-	InfoPopover = require( 'components/info-popover' ),
-	stats = require( 'lib/posts/stats' );
+import EditorFieldset from 'post-editor/editor-fieldset';
+import FormCheckbox from 'components/forms/form-checkbox';
+import PostActions from 'lib/posts/actions';
+import InfoPopover from 'components/info-popover';
+import stats from 'lib/posts/stats';
 
 function booleanToStatus( bool ) {
 	return bool ? 'open' : 'closed';

client/post-editor/editor-location/index.jsx
@@ -1,18 +1,17 @@
 /**
  * External dependencies
  */
-const PropTypes = require( 'prop-types' );
-const React = require( 'react' ),
-	qs = require( 'querystring' );
+import PropTypes from 'prop-types';
+import React from 'react';
+import qs from 'querystring';
 
 /**
  * Internal dependencies
  */
-const PostActions = require( 'lib/posts/actions' ),
-	EditorDrawerWell = require( 'post-editor/editor-drawer-well' ),
-	stats = require( 'lib/posts/stats' ),
-	EditorLocationSearch = require( './search' );
-
+import PostActions from 'lib/posts/actions';
+import EditorDrawerWell from 'post-editor/editor-drawer-well';
+import stats from 'lib/posts/stats';
+import EditorLocationSearch from './search';
 import Notice from 'components/notice';
 
 /**

client/account-recovery/forgot-username-form/index.jsx
@@ -13,7 +13,6 @@ import Card from 'components/card';
 import FormButton from 'components/forms/form-button';
 import FormLabel from 'components/forms/form-label';
 import FormInput from 'components/forms/form-text-input';
-
 import { fetchResetOptionsByNameAndUrl } from 'state/account-recovery/reset/actions';
 
 import {

client/account-recovery/lost-password-form/index.jsx
@@ -14,7 +14,6 @@ import Card from 'components/card';
 import FormButton from 'components/button';
 import FormLabel from 'components/forms/form-label';
 import FormInput from 'components/forms/form-text-input';
-
 import { fetchResetOptionsByLogin } from 'state/account-recovery/reset/actions';
 
 import {

client/account-recovery/reset-password-form/index.jsx
@@ -15,7 +15,6 @@ import FormFieldset from 'components/forms/form-fieldset';
 import FormLegend from 'components/forms/form-legend';
 import ResetOptionSet from './reset-option-set';
 import ErrorMessage from 'account-recovery/components/account-recovery-error-message';
-
 import {
 	setResetMethod,
 	requestReset,

client/account-recovery/reset-password-sms-form/index.jsx
@@ -13,7 +13,6 @@ import Card from 'components/card';
 import FormTextInput from 'components/forms/form-text-input';
 import FormButton from 'components/forms/form-button';
 import ErrorMessage from 'account-recovery/components/account-recovery-error-message';
-
 import {
 	getAccountRecoveryResetUserData,
 	getAccountRecoveryResetSelectedMethod,

client/blocks/plan-storage/index.jsx
@@ -17,7 +17,6 @@ import {
 	isJetpackSite
 } from 'state/sites/selectors';
 import { PLAN_BUSINESS } from 'lib/plans/constants';
-
 import PlanStorageBar from './bar';
 
 class PlanStorage extends Component {

client/blocks/post-share/index.jsx
@@ -61,14 +61,11 @@ import {
 	PLAN_JETPACK_PREMIUM
 } from 'lib/plans/constants';
 import { UpgradeToPremiumNudge } from 'blocks/post-share/nudges';
-
 import SharingPreviewModal from './sharing-preview-modal';
 import ConnectionsList, { NoConnectionsNotice } from './connections-list';
-
 import ActionsList from './publicize-actions-list';
 import CalendarButton from 'blocks/calendar-button';
 import EventsTooltip from 'components/date-picker/events-tooltip';
-
 import SectionHeader from 'components/section-header';
 import Tooltip from 'components/tooltip';
 import analytics from 'lib/analytics';

commit 2a38b2de87c1001a8d4d7d74e758af857cfab74a

Post Editor: Fix stats imports (#18572)
Broken by #18359, reported by @rachelmcr in Slack.

>When I change the discussion settings (on a post) I get `Uncaught TypeError: Cannot read property 'recordStat' of undefined`

client/post-editor/editor-discussion/index.jsx
@@ -12,7 +12,7 @@ import EditorFieldset from 'post-editor/editor-fieldset';
 import FormCheckbox from 'components/forms/form-checkbox';
 import PostActions from 'lib/posts/actions';
 import InfoPopover from 'components/info-popover';
-import stats from 'lib/posts/stats';
+import { recordEvent, recordStat } from 'lib/posts/stats';
 
 function booleanToStatus( bool ) {
 	return bool ? 'open' : 'closed';
@@ -75,8 +75,8 @@ export default React.createClass( {
 			gaEvent = 'Trackback status changed';
 		}
 
-		stats.recordStat( statName );
-		stats.recordEvent( gaEvent, newStatus );
+		recordStat( statName );
+		recordEvent( gaEvent, newStatus );
 
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 		PostActions.edit( {

client/post-editor/editor-location/index.jsx
@@ -10,7 +10,7 @@ import qs from 'querystring';
  */
 import PostActions from 'lib/posts/actions';
 import EditorDrawerWell from 'post-editor/editor-drawer-well';
-import stats from 'lib/posts/stats';
+import { recordEvent, recordStat } from 'lib/posts/stats';
 import EditorLocationSearch from './search';
 import Notice from 'components/notice';
 
@@ -49,7 +49,7 @@ export default React.createClass( {
 			geo_longitude: position.coords.longitude
 		} );
 
-		stats.recordStat( 'location_geolocate_success' );
+		recordStat( 'location_geolocate_success' );
 	},
 
 	onGeolocateFailure: function( error ) {
@@ -58,7 +58,7 @@ export default React.createClass( {
 			locating: false
 		} );
 
-		stats.recordStat( 'location_geolocate_failed' );
+		recordStat( 'location_geolocate_failed' );
 	},
 
 	resetError: function() {
@@ -79,8 +79,8 @@ export default React.createClass( {
 			{ enableHighAccuracy: true }
 		);
 
-		stats.recordStat( 'location_geolocate' );
-		stats.recordEvent( 'Location Geolocated' );
+		recordStat( 'location_geolocate' );
+		recordEvent( 'Location Geolocated' );
 	},
 
 	clear: function() {