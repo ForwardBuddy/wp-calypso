ForwardBuddy/wp-calypso

commit ac0660a7a27debace40aed6e97d422c0d2aab12c

Remove p elements to avoid divs inside p
Fixes warning: "validateDOMNesting(...): <div> cannot appear as a descendant of <p>."

client/gutenberg/extensions/mailchimp/edit.jsx
@@ -190,19 +190,15 @@ class MailchimpSubscribeEdit extends Component {
 		);
 		const blockContent = (
 			<div className={ classes }>
-				<p>
-					<TextControl
-						aria-label={ emailPlaceholder }
-						disabled
-						onChange={ () => false }
-						placeholder={ emailPlaceholder }
-						title={ __( 'You can edit the email placeholder in the sidebar.' ) }
-						type="email"
-					/>
-				</p>
-				<p>
-					<SubmitButton { ...this.props } />
-				</p>
+				<TextControl
+					aria-label={ emailPlaceholder }
+					disabled
+					onChange={ () => false }
+					placeholder={ emailPlaceholder }
+					title={ __( 'You can edit the email placeholder in the sidebar.' ) }
+					type="email"
+				/>
+				<SubmitButton { ...this.props } />
 				<RichText
 					tagName="p"
 					placeholder={ __( 'Write consent text' ) }

commit 1d8bad6c73c22d7e64681066010052c02dd7f298

Mailchimp: Markup Improvements (#30970)
* Wrap input and submit button in P tags.

* For consent text, replace SMALL with P tags.

* Role attribute for notifications.

* LABEL and aria-label for email input field.

* Editable submit button: remove invalid wrapping DIV, change tag to SPAN, apply class to the SPAN instead of wrapper.

* Remove bold styling, which leads to unexpected STRONG tags in submit button text.

* Linting fix: extra space in style.

* Unique instance ID for email input field.

* Change submit button label attribute name to "submitButtonText" in preparation for using the SubmitButton component which requires that attribute name.

* Replace submit button with shared SubmitButton component. Remove all unnecessary button styling.

* Remove FORM tag, which was creating problems with giving focus to the Submit Button when there is no text in it.

* Submit button text and background color customization.

* HtmlFor instead of for, because of https://reactjs.org/docs/dom-elements.html#htmlfor

* Hide form elements by CSS when auditioning notifications.

* Remove reduntant `label` and instanceId

* Remove p elements to avoid divs inside p

Fixes warning: "validateDOMNesting(...): <div> cannot appear as a descendant of <p>."

* Fix spacing to satisfy stylelint

* Fix hiding notification when modifying notification labels

Caused by https://github.com/Automattic/wp-calypso/commit/ac0660a7a27debace40aed6e97d422c0d2aab12c

* Removing styles for hidden LABEL, which has been replaced by aria-label.

client/gutenberg/extensions/mailchimp/edit.jsx
@@ -3,6 +3,8 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import classnames from 'classnames';
+import SubmitButton from 'gutenberg/extensions/presets/jetpack/utils/submit-button';
 import {
 	Button,
 	ExternalLink,
@@ -118,17 +120,17 @@ class MailchimpSubscribeEdit extends Component {
 		return null;
 	};
 
+	roleForAuditionType = audition => {
+		if ( audition === NOTIFICATION_ERROR ) {
+			return 'alert';
+		}
+		return 'status';
+	};
+
 	render = () => {
 		const { attributes, className, notices, noticeUI, setAttributes } = this.props;
 		const { audition, connected, connectURL } = this.state;
-		const {
-			emailPlaceholder,
-			submitLabel,
-			consentText,
-			processingLabel,
-			successLabel,
-			errorLabel,
-		} = attributes;
+		const { emailPlaceholder, consentText, processingLabel, successLabel, errorLabel } = attributes;
 		const classPrefix = 'wp-block-jetpack-mailchimp_';
 		const waiting = (
 			<Placeholder icon={ icon } notices={ notices }>
@@ -182,36 +184,32 @@ class MailchimpSubscribeEdit extends Component {
 				</PanelBody>
 			</InspectorControls>
 		);
+		const blockClasses = classnames( className, {
+			[ `${ classPrefix }notication-audition` ]: audition,
+		} );
 		const blockContent = (
-			<div className={ className }>
-				{ ! audition && (
-					<form ref={ this.formRef }>
-						<TextControl
-							disabled
-							placeholder={ emailPlaceholder }
-							onChange={ () => false }
-							title={ __( 'You can edit the email placeholder in the sidebar.' ) }
-							type="email"
-						/>
-						<div className="wp-block-jetpack-mailchimp_button">
-							<RichText
-								formattingControls={ [] }
-								placeholder={ __( 'Add button text…' ) }
-								value={ submitLabel }
-								onChange={ value => setAttributes( { submitLabel: value } ) }
-							/>
-						</div>
-						<RichText
-							tagName="small"
-							placeholder={ __( 'Write consent text' ) }
-							value={ consentText }
-							onChange={ value => setAttributes( { consentText: value } ) }
-							inlineToolbar
-						/>
-					</form>
-				) }
+			<div className={ blockClasses }>
+				<TextControl
+					aria-label={ emailPlaceholder }
+					disabled
+					onChange={ () => false }
+					placeholder={ emailPlaceholder }
+					title={ __( 'You can edit the email placeholder in the sidebar.' ) }
+					type="email"
+				/>
+				<SubmitButton { ...this.props } />
+				<RichText
+					tagName="p"
+					placeholder={ __( 'Write consent text' ) }
+					value={ consentText }
+					onChange={ value => setAttributes( { consentText: value } ) }
+					inlineToolbar
+				/>
 				{ audition && (
-					<div className={ `${ classPrefix }notification ${ classPrefix }${ audition }` }>
+					<div
+						className={ `${ classPrefix }notification ${ classPrefix }${ audition }` }
+						role={ this.roleForAuditionType( audition ) }
+					>
 						{ this.labelForAuditionType( audition ) }
 					</div>
 				) }

client/gutenberg/extensions/mailchimp/editor.scss
@@ -1,47 +1,21 @@
 @import './view.scss';
 
-// Variables from the https://github.com/WordPress/gutenberg to maintain style consistency with core Button block
-$big-font-size: 18px;
-$blocks-button__height: 46px;
-$blocks-button__line-height: $big-font-size + 6px;
-
 .wp-block-jetpack-mailchimp {
 
 	.wp-block-jetpack-mailchimp_notification {
 		display: block;
 	}
 
-	.wp-block-jetpack-mailchimp_button {
-		background-color: #0073aa;
-		border-radius: 5px;
-		color: var( --color-white );
-		font-family: $sans;
-		font-weight: bold;
-		margin-bottom: 1.5em;
-
-		// Styles from the Gutenberg repo to maintain style consistency with core Button block
-		border: none;
-		box-shadow: none;
-		cursor: pointer;
-		display: inline-block;
-		font-size: $big-font-size;
-		line-height: $blocks-button__line-height;
-		margin: 0;
-		padding: ($blocks-button__height - $blocks-button__line-height) / 2 24px;
-		text-align: center;
-		text-decoration: none;
-		white-space: normal;
-		word-break: break-all;
-		.editor-rich-text__tinymce {
-			cursor: text;
+	.editor-rich-text__inline-toolbar {
+		pointer-events: none;
+		.components-toolbar {
+			pointer-events: all;
 		}
 	}
 
-	.editor-rich-text__inline-toolbar {
-		pointer-events: none;
-		.components-toolbar  {
-			pointer-events: all;
-		}
+	// Hide everything else except notification when modifying notification labels
+	&.wp-block-jetpack-mailchimp_notication-audition > *:not( .wp-block-jetpack-mailchimp_notification ) {
+		display: none;
 	}
 
 }

client/gutenberg/extensions/mailchimp/index.js
@@ -34,9 +34,15 @@ export const settings = {
 			type: 'string',
 			default: __( 'Enter your email' ),
 		},
-		submitLabel: {
+		submitButtonText: {
 			type: 'string',
 			default: __( 'Join my email list' ),
+		},
+		customBackgroundButtonColor: {
+			type: 'string',
+		},
+		customTextButtonColor: {
+			type: 'string',
 		},
 		consentText: {
 			type: 'string',