ForwardBuddy/wp-calypso

commit 0b7f17a9244180928d569b47de871f98654fea22

Don't instantiate user at module level (#43069)
* Don't instantiate user at module level

`lib/user` is part of several circular dependencies, so instantiation at
the top level can lead to hard-to-find initialisation order problems.
Since `lib/user` follows a singleton pattern anyway, there's no need for
performance considerations, and we can just invoke the default export
as often as needed.

* Account for server replacing `lib/user` with noop

* Use `get()` instead of `data`.

* Switch to `currentUser` state in components

client/me/purchases/controller.jsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import { noop } from 'lodash';
 import React from 'react';
 
@@ -21,18 +20,16 @@ import PurchasesList from './purchases-list';
 import { concatTitle } from 'lib/react-helpers';
 import { setDocumentHeadTitle } from 'state/document-head/actions';
 import titles from './titles';
-import userFactory from 'lib/user';
 import { makeLayout, render as clientRender } from 'controller';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
-
-const user = userFactory();
+import { getCurrentUserSiteCount } from 'state/current-user/selectors';
 
 // FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 function setTitle( context, ...title ) {
 	context.store.dispatch( setDocumentHeadTitle( concatTitle( titles.purchases, ...title ) ) );
 }
 
-const userHasNoSites = () => user.get().site_count <= 0;
+const userHasNoSites = ( state ) => getCurrentUserSiteCount( state ) <= 0;
 
 function noSites( context, analyticsPath ) {
 	setTitle( context );
@@ -48,7 +45,9 @@ function noSites( context, analyticsPath ) {
 }
 
 export function addCardDetails( context, next ) {
-	if ( userHasNoSites() ) {
+	const state = context.store.getState();
+
+	if ( userHasNoSites( state ) ) {
 		return noSites( context, '/me/purchases/:site/:purchaseId/payment/add' );
 	}
 

client/extensions/woocommerce/app/dashboard/setup/store-location.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
@@ -34,10 +33,8 @@ import { doInitialSetup } from 'woocommerce/state/sites/settings/actions';
 import QueryContactDetailsCache from 'components/data/query-contact-details-cache';
 import QueryLocations from 'woocommerce/components/query-locations';
 import QuerySettingsGeneral from 'woocommerce/components/query-settings-general';
-import userFactory from 'lib/user';
+import user from 'lib/user';
 import VerifyEmailDialog from 'components/email-verification/email-verification-dialog';
-
-const user = userFactory();
 
 class StoreLocationSetupView extends Component {
 	constructor( props ) {
@@ -275,9 +272,9 @@ class StoreLocationSetupView extends Component {
 	closeVerifyEmailDialog = () => {
 		this.setState( { showEmailVerificationDialog: false } );
 		// Re-fetch the user to see if they actually took care of things
-		user.fetch();
+		user().fetch();
 		this.setState( { isFetchingUser: true } );
-		user.once( 'change', () => this.setState( { isFetchingUser: false } ) );
+		user().once( 'change', () => this.setState( { isFetchingUser: false } ) );
 	};
 
 	render = () => {

client/lib/abtest/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import debugFactory from 'debug';
-import { every, get, includes, isArray, keys, reduce, some } from 'lodash';
+import { every, includes, isArray, keys, reduce, some } from 'lodash';
 import store from 'store';
 import { getLocaleSlug } from 'i18n-calypso';
 
@@ -11,13 +11,12 @@ import { getLocaleSlug } from 'i18n-calypso';
  */
 import activeTests from 'lib/abtest/active-tests';
 import { recordTracksEvent } from 'lib/analytics/tracks';
-import userFactory from 'lib/user';
+import user from 'lib/user';
 import wpcom from 'lib/wp';
 import { ABTEST_LOCALSTORAGE_KEY } from 'lib/abtest/utility';
 import { getLanguageSlugs } from 'lib/i18n-utils/utils';
 
 const debug = debugFactory( 'calypso:abtests' );
-const user = userFactory();
 
 function ABTest( name, geoLocation ) {
 	if ( ! ( this instanceof ABTest ) ) {
@@ -64,7 +63,7 @@ export const saveABTestVariation = ( name, variation ) =>
 
 export const getAllTests = () => keys( activeTests ).map( ABTest );
 
-const isUserSignedIn = () => user && user.get() !== false;
+const isUserSignedIn = () => user() && user().get() !== false;
 
 const parseDateStamp = ( datestamp ) => {
 	const format = 'YYYYMMDD';
@@ -209,7 +208,7 @@ export const isUsingGivenLocales = ( localeTargets, experimentId = null ) => {
 		client.languages && client.languages.length ? client.languages[ 0 ] : 'en';
 	const localeFromSession = getLocaleSlug() || 'en';
 	const localeMatcher = new RegExp( '^(' + localeTargets.join( '|' ) + ')', 'i' );
-	const userLocale = user.get().localeSlug || 'en';
+	const userLocale = user().get().localeSlug || 'en';
 
 	if ( isUserSignedIn() && ! userLocale.match( localeMatcher ) ) {
 		debug( '%s: User has a %s locale', experimentId, userLocale );
@@ -300,7 +299,7 @@ ABTest.prototype.hasBeenInPreviousSeriesTest = function () {
 };
 
 ABTest.prototype.hasRegisteredBeforeTestBegan = function () {
-	return user && user.get() && new Date( user.get().date ) < new Date( this.startDate );
+	return user() && user().get() && new Date( user().get().date ) < new Date( this.startDate );
 };
 
 ABTest.prototype.getSavedVariation = function () {
@@ -311,7 +310,7 @@ ABTest.prototype.assignVariation = function () {
 	let variationName, randomAllocationAmount;
 	let sum = 0;
 
-	const userId = get( user, 'data.ID' );
+	const userId = user()?.get()?.ID;
 	const allocationsTotal = reduce(
 		this.variationDetails,
 		( allocations, allocation ) => {
@@ -321,7 +320,7 @@ ABTest.prototype.assignVariation = function () {
 	);
 
 	if ( this.assignmentMethod === 'userId' && ! isNaN( +userId ) ) {
-		randomAllocationAmount = Number( user.data.ID ) % allocationsTotal;
+		randomAllocationAmount = Number( userId ) % allocationsTotal;
 	} else {
 		randomAllocationAmount = Math.random() * allocationsTotal;
 	}

client/lib/desktop/index.js
@@ -3,14 +3,11 @@
  */
 import debugFactory from 'debug';
 
-const debug = debugFactory( 'calypso:desktop' );
-
 /**
  * Internal dependencies
  */
 import { newPost } from 'lib/paths';
-import userFactory from 'lib/user';
-const user = userFactory();
+import user from 'lib/user';
 import { ipcRenderer as ipc } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
 import * as oAuthToken from 'lib/oauth-token';
 import userUtilities from 'lib/user/utils';
@@ -32,6 +29,7 @@ import { requestSite } from 'state/sites/actions';
 /**
  * Module variables
  */
+const debug = debugFactory( 'calypso:desktop' );
 
 const Desktop = {
 	/**
@@ -104,14 +102,14 @@ const Desktop = {
 	sendUserLoginStatus: function () {
 		let status = true;
 
-		if ( user.data === false || user.data instanceof Array ) {
+		if ( user().get() === false ) {
 			status = false;
 		}
 
 		debug( 'Sending logged-in = ' + status );
 
 		ipc.send( 'user-login-status', status );
-		ipc.send( 'user-auth', user, oAuthToken.getToken() );
+		ipc.send( 'user-auth', user(), oAuthToken.getToken() );
 	},
 
 	onToggleNotifications: function () {

client/lib/signup/step-actions/fetch-sites-and-user.js
@@ -6,7 +6,7 @@ import debugFactory from 'debug';
 /**
  * Internal dependencies
  */
-import userFactory from 'lib/user';
+import user from 'lib/user';
 
 // State actions and selectors
 import getSiteId from 'state/selectors/get-site-id';
@@ -16,7 +16,6 @@ import { promisify } from 'utils';
 /**
  * Constants
  */
-const user = userFactory();
 const debug = debugFactory( 'calypso:signup:step-actions:fetch-sites-and-user' );
 
 function fetchSitesUntilSiteAppears( siteSlug, reduxStore, callback ) {
@@ -38,8 +37,8 @@ export function fetchSitesAndUser( siteSlug, onComplete, reduxStore ) {
 	Promise.all( [
 		promisify( fetchSitesUntilSiteAppears )( siteSlug, reduxStore ),
 		new Promise( ( resolve ) => {
-			user.once( 'change', resolve );
-			user.fetch();
+			user().once( 'change', resolve );
+			user().fetch();
 		} ),
 	] ).then( onComplete );
 }

client/lib/signup/step-actions/index.js
@@ -24,9 +24,7 @@ import { parse as parseURL } from 'url';
 // Libraries
 import wpcom from 'lib/wp';
 import guessTimezone from 'lib/i18n-utils/guess-timezone';
-
-/* eslint-enable no-restricted-imports */
-import userFactory from 'lib/user';
+import user from 'lib/user';
 import { getSavedVariations } from 'lib/abtest';
 import { recordTracksEvent } from 'lib/analytics/tracks';
 import { recordRegistration } from 'lib/analytics/signup';
@@ -67,7 +65,6 @@ import { fetchSitesAndUser } from 'lib/signup/step-actions/fetch-sites-and-user'
 /**
  * Constants
  */
-const user = userFactory();
 const debug = debugFactory( 'calypso:signup:step-actions' );
 
 export function createSiteOrDomain( callback, dependencies, data, reduxStore ) {
@@ -205,7 +202,7 @@ export function createSiteWithCart( callback, dependencies, stepData, reduxStore
 
 	if ( shouldSkipDomainStep || shouldHideFreePlan || shouldHideDomainStep ) {
 		newSiteParams.blog_name =
-			get( user.get(), 'username' ) ||
+			user().get()?.username ||
 			get( signupDependencies, 'username' ) ||
 			siteTitle ||
 			siteType ||
@@ -332,15 +329,15 @@ function processItemCart(
 		}
 	};
 
-	if ( ! user.get() && isFreeThemePreselected ) {
+	if ( ! user().get() && isFreeThemePreselected ) {
 		setThemeOnSite( addToCartAndProceed, { siteSlug, themeSlugWithRepo } );
-	} else if ( user.get() && isFreeThemePreselected ) {
+	} else if ( user().get() && isFreeThemePreselected ) {
 		fetchSitesAndUser(
 			siteSlug,
 			setThemeOnSite.bind( null, addToCartAndProceed, { siteSlug, themeSlugWithRepo } ),
 			reduxStore
 		);
-	} else if ( user.get() ) {
+	} else if ( user().get() ) {
 		fetchSitesAndUser( siteSlug, addToCartAndProceed, reduxStore );
 	} else {
 		addToCartAndProceed();
@@ -534,7 +531,7 @@ export function createSite( callback, dependencies, stepData, reduxStore ) {
 			providedDependencies = { siteSlug };
 		}
 
-		if ( user.get() && isEmpty( errors ) ) {
+		if ( user().get() && isEmpty( errors ) ) {
 			fetchSitesAndUser( siteSlug, () => callback( undefined, providedDependencies ), reduxStore );
 		} else {
 			callback( isEmpty( errors ) ? undefined : [ errors ], providedDependencies );
@@ -571,7 +568,7 @@ export function createWpForTeamsSite( callback, dependencies, stepData, reduxSto
 			providedDependencies = { siteSlug };
 		}
 
-		if ( user.get() && isEmpty( errors ) ) {
+		if ( user().get() && isEmpty( errors ) ) {
 			fetchSitesAndUser( siteSlug, () => callback( undefined, providedDependencies ), reduxStore );
 		} else {
 			callback( isEmpty( errors ) ? undefined : [ errors ], providedDependencies );

client/lib/translator-jumpstart/index.js
@@ -12,14 +12,13 @@ import { find, isUndefined } from 'lodash';
  */
 import { languages } from 'languages';
 import { loadjQueryDependentScriptDesktopWrapper } from 'lib/load-jquery-dependent-script-desktop-wrapper';
-import User from 'lib/user';
+import user from 'lib/user';
 import { recordTracksEvent } from 'lib/analytics/tracks';
 import { canBeTranslated } from 'lib/i18n-utils';
 
 const debug = debugModule( 'calypso:community-translator' );
 
-const user = new User(),
-	communityTranslatorBaseUrl = 'https://widgets.wp.com/community-translator/',
+const communityTranslatorBaseUrl = 'https://widgets.wp.com/community-translator/',
 	communityTranslatorVersion = '1.160729',
 	// lookup for the translation set slug on GP
 	translateSetSlugs = {
@@ -54,7 +53,7 @@ let injectUrl,
  */
 const communityTranslatorJumpstart = {
 	isEnabled() {
-		const currentUser = user.get();
+		const currentUser = user().get();
 
 		// disable for locales
 		if (
@@ -188,7 +187,7 @@ const communityTranslatorJumpstart = {
 			languageJson[ '' ][ 'Plural-Forms' ] ||
 			languageJson[ '' ][ 'plural-forms' ] ||
 			translationDataFromPage.pluralForms;
-		translationDataFromPage.currentUserId = user.data.ID;
+		translationDataFromPage.currentUserId = user().get().ID;
 
 		const currentLocale = find( languages, ( lang ) => lang.langSlug === localeCode );
 		if ( currentLocale ) {
@@ -323,7 +322,7 @@ export function trackTranslatorStatus( isTranslatorEnabled ) {
 
 	if ( changed && _isTranslatorEnabled !== undefined ) {
 		debug( tracksEvent );
-		recordTracksEvent( tracksEvent, { locale: user.data.localeSlug } );
+		recordTracksEvent( tracksEvent, { locale: user().get().localeSlug } );
 	}
 
 	_isTranslatorEnabled = newSetting;

client/lib/user-settings/index.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import { assign, isEmpty, keys, merge, has, get, set, unset } from 'lodash';
 import debugFactory from 'debug';
 const debug = debugFactory( 'calypso:user:settings' );
@@ -11,8 +10,7 @@ import { decodeEntities } from 'lib/formatting';
  * Internal dependencies
  */
 import emitterClass from 'lib/mixins/emitter';
-import userFactory from 'lib/user';
-const user = userFactory();
+import user from 'lib/user';
 import userUtils from 'lib/user/utils';
 import wp from 'lib/wp';
 
@@ -178,7 +176,7 @@ UserSettings.prototype.saveSettings = function ( callback, settingsOverride ) {
 					this.emit( 'change' );
 
 					// Refetch the user data after saving user settings
-					user.fetch();
+					user().fetch();
 				}
 
 				// Let the form know whether the save was successful or not

commit 360bed41e04b72170cba222b101b605b61896003

Fix confirmCancelDomain and editCardDetails controllers to properly pass the state to userHasNoSites (#43143)
Fixing a regression introduced in #43069 with confirmCancelDomain and editCardDetails controllers

client/me/purchases/controller.jsx
@@ -80,7 +80,9 @@ export function cancelPurchase( context, next ) {
 }
 
 export function confirmCancelDomain( context, next ) {
-	if ( userHasNoSites() ) {
+	const state = context.store.getState();
+
+	if ( userHasNoSites( state ) ) {
 		return noSites( context, '/me/purchases/:site/:purchaseId/confirm-cancel-domain' );
 	}
 
@@ -96,7 +98,9 @@ export function confirmCancelDomain( context, next ) {
 }
 
 export function editCardDetails( context, next ) {
-	if ( userHasNoSites() ) {
+	const state = context.store.getState();
+
+	if ( userHasNoSites( state ) ) {
 		return noSites( context, '/me/purchases/:site/:purchaseId/payment/edit/:cardId' );
 	}
 