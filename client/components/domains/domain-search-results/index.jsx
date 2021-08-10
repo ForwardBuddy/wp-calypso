ForwardBuddy/wp-calypso

commit c4b4ae8903f02a1110734c712ebec39aa87a57bb

Add better availability message when no site is selected (#55159)

client/components/domains/domain-search-results/index.jsx
@@ -172,6 +172,18 @@ class DomainSearchResults extends React.Component {
 						}
 				  );
 
+			if ( ! selectedSite ) {
+				domainUnavailableMessage = translate(
+					'{{strong}}%(domain)s{{/strong}} is already registered. Please try another search.',
+					{
+						args: { domain },
+						components: {
+							strong: <strong />,
+						},
+					}
+				);
+			}
+
 			if ( TLD_NOT_SUPPORTED_TEMPORARILY === lastDomainStatus ) {
 				domainUnavailableMessage = translate(
 					'{{strong}}.%(tld)s{{/strong}} domains are temporarily not offered on WordPress.com. ' +

commit 101c058f3277ddd346b1b4e72a4dc10fa29da596

Fix how we render notices in the domain suggestion screen in signup (#55234)
Recently we've updated the notice we show when you enter a domain that is already registered in the suggestion screen ( see #54926 ) but it wasn't looking good in the signup flow so here's a fix for that. There was another issue that was introduced in #55159 that we're also fixing here

client/components/domains/domain-search-results/index.jsx
@@ -72,6 +72,7 @@ class DomainSearchResults extends React.Component {
 			lastDomainSearched,
 			selectedSite,
 			translate,
+			isDomainOnly,
 		} = this.props;
 		const availabilityElementClasses = classNames( {
 			'domain-search-results__domain-is-available': availableDomain,
@@ -162,7 +163,7 @@ class DomainSearchResults extends React.Component {
 						}
 				  );
 
-			if ( ! selectedSite ) {
+			if ( isDomainOnly && ! [ TLD_NOT_SUPPORTED, UNKNOWN ].includes( lastDomainStatus ) ) {
 				domainUnavailableMessage = translate(
 					'{{strong}}%(domain)s{{/strong}} is already registered. Please try another search.',
 					{

client/components/domains/featured-domain-suggestions/style.scss
@@ -218,7 +218,7 @@ body.is-section-signup.is-white-signup .step-wrapper .featured-domain-suggestion
 
 	&.featured-domain-suggestion--is-placeholder {
 		margin-bottom: 12px;
-		margin-top: 40px;
+		margin-top: 0;
 		border-radius: 4px; /* stylelint-disable-line */
 	}
 }

client/components/domains/register-domain-step/style.scss
@@ -7,12 +7,16 @@
 	&.register-domain-step__search-domain-step:not( .is-sticky ) {
 		padding-bottom: 24px;
 
+		.is-white-signup & {
+			padding-bottom: 16px;
+		}
+
 		@include breakpoint-deprecated( '>660px' ) {
 			padding-top: 18px;
 
 			.is-white-signup & {
 				padding-top: 0;
-				padding-bottom: 20px;
+				padding-bottom: 16px;
 			}
 		}
 	}
@@ -124,7 +128,6 @@ button.register-domain-step__next-page-button {
 	font-weight: 500; /* stylelint-disable-line */
 }
 
-
 body.is-section-domains {
 	.register-domain-step {
 		button.register-domain-step__next-page-button {
@@ -136,6 +139,21 @@ body.is-section-domains {
 }
 
 body.is-section-signup.is-white-signup {
+	.domain-search-results__domain-availability .card {
+		box-shadow: none;
+		padding: 0;
+		margin-bottom: 24px;
+		margin-left: 20px;
+
+		@include break-mobile {
+			margin-left: 0;
+		}
+	}
+	.domain-search-results__domain-availability svg,
+	.register-domain-step__notice svg {
+		margin: 0;
+		padding: 0;
+	}
 	.register-domain-step__next-page {
 		background: none;
 		box-shadow: none;
@@ -155,4 +173,3 @@ body.is-section-signup.is-white-signup {
 		}
 	}
 }
-