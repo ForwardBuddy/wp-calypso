ForwardBuddy/wp-calypso

commit bbb1f4faa19659708ae274a49daf3c06bf82ced4

Virtual themes i1: Implement A/B test (#73663)

client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
@@ -22,6 +22,7 @@ import PremiumGlobalStylesUpgradeModal from 'calypso/components/premium-global-s
 import WebPreview from 'calypso/components/web-preview/content';
 import { useSiteVerticalQueryById } from 'calypso/data/site-verticals';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { useExperiment } from 'calypso/lib/explat';
 import { urlToSlug } from 'calypso/lib/url';
 import { usePremiumGlobalStyles } from 'calypso/state/sites/hooks/use-premium-global-styles';
 import { requestActiveTheme } from 'calypso/state/themes/actions';
@@ -120,16 +121,20 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow } ) => {
 		return allDesigns;
 	};
 
+	const [ isLoadingVirtualThemesExperiment, virtualThemesExperiment ] = useExperiment(
+		'calypso_signup_design_picker_virtual_themes_v1'
+	);
+
 	const { data: allDesigns, isLoading: isLoadingDesigns } = useStarterDesignsQuery(
 		{
 			vertical_id: siteVerticalId,
 			intent,
 			seed: siteSlugOrId || undefined,
 			_locale: locale,
-			include_virtual_designs: isEnabled( 'virtual-themes/onboarding' ),
+			include_virtual_designs: virtualThemesExperiment?.variationName === 'treatment',
 		},
 		{
-			enabled: true,
+			enabled: ! isLoadingVirtualThemesExperiment,
 			select: selectStarterDesigns,
 			shouldLimitGlobalStyles,
 		}

commit b74f11ad171f36cceb343836f47aa857a61ecb4e

Prevent calypso_signup_design_scrolled_to_end from being tracked if designs have not been loaded (#73989)
Fixes a regression introduced by https://github.com/Automattic/wp-calypso/pull/73663 that caused the `calypso_signup_design_scrolled_to_end` event to be tracked without the user viewing all the designs.

The culprit is that we need to determine first the assignment group of the virtual themes i1 experiment and then fetch the designs based on that group. During that time, we render an empty page so the user technically has reached the end of the page and thus `calypso_signup_design_scrolled_to_end` gets tracked. However the event is intended to be triggered when a user has scrolled to the end of the page AFTER viewing all designs.

So this commit mitigates the issue by adding a safeguard to prevent the component from being rendered (effectively  blocking the `calypso_signup_design_scrolled_to_end` event) if we're still loading the A/B test group.

Props to @taipeicoder for finding out the culprit (and to @ianstewart for noting the spike).

client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
@@ -581,7 +581,7 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow } ) => {
 	// ********** Main render logic
 
 	// Don't render until we've done fetching all the data needed for initial render.
-	if ( ! site || isLoadingSiteVertical || isLoadingDesigns ) {
+	if ( ! site || isLoadingSiteVertical || isLoadingDesigns || isLoadingVirtualThemesExperiment ) {
 		return null;
 	}
 

client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/test/unified-design-picker.tsx
@@ -43,6 +43,10 @@ jest.mock( 'calypso/state/sites/hooks/use-premium-global-styles', () => ( {
 	usePremiumGlobalStyles: () => ( { shouldLimitGlobalStyles: false } ),
 } ) );
 
+jest.mock( 'calypso/lib/explat', () => ( {
+	useExperiment: () => [ false, null ],
+} ) );
+
 /**
  * Mock wpcom-proxy-request so that we could use wpcom-xhr-request to call the endpoint
  * and get the response from nock