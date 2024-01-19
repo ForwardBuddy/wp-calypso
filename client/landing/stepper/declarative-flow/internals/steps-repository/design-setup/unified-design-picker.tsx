ForwardBuddy/wp-calypso

commit 9c7f7b49c19c00e766dc4a5003a0c4f27a32b8fd

Theme Tiers: Design Picker aware of Personal Themes (#86407)
* The Design Picker is now aware of themes with Personal tier and shhows the upgrade modal with the corresponding checkout plan selected

* Deleted un-needed prop

* Added feature check

* The design picker is now aware of when a personal/starter plan has been purchased

* Simplified a condition

client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
@@ -1,5 +1,9 @@
 import { isEnabled } from '@automattic/calypso-config';
-import { PLAN_BUSINESS_MONTHLY, WPCOM_FEATURES_PREMIUM_THEMES } from '@automattic/calypso-products';
+import {
+	PLAN_BUSINESS_MONTHLY,
+	PLAN_PERSONAL,
+	WPCOM_FEATURES_PREMIUM_THEMES,
+} from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import {
 	Onboard,
@@ -14,6 +18,8 @@ import {
 	getDesignPreviewUrl,
 	isAssemblerDesign,
 	isAssemblerSupported,
+	FREE_THEME,
+	PERSONAL_THEME,
 } from '@automattic/design-picker';
 import { useLocale } from '@automattic/i18n-utils';
 import { StepContainer, DESIGN_FIRST_FLOW } from '@automattic/onboarding';
@@ -48,6 +54,8 @@ import {
 	isMarketplaceThemeSubscribed as getIsMarketplaceThemeSubscribed,
 	getTheme,
 	isSiteEligibleForManagedExternalThemes,
+	getThemeTierForTheme,
+	isThemeAllowedOnSite,
 } from 'calypso/state/themes/selectors';
 import { isThemePurchased } from 'calypso/state/themes/selectors/is-theme-purchased';
 import { getPreferredBillingCycleProductSlug } from 'calypso/state/themes/theme-utils';
@@ -370,11 +378,19 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 			( product ) => product.product_slug === marketplaceProductSlug
 		) || marketplaceThemeProducts[ 0 ];
 
-	const didPurchaseSelectedTheme = useSelector( ( state ) =>
-		site && selectedDesignThemeId
-			? isThemePurchased( state, selectedDesignThemeId, site.ID )
-			: false
-	);
+	const didPurchaseSelectedTheme = useSelector( ( state ) => {
+		if ( ! site || ! selectedDesignThemeId ) {
+			return false;
+		}
+
+		if ( isEnabled( 'themes/tiers' ) ) {
+			return isThemeAllowedOnSite( state, site.ID, selectedDesignThemeId );
+		}
+
+		// @TODO Remove this once we have the new theme tiers live.
+		return isThemePurchased( state, selectedDesignThemeId, site.ID );
+	} );
+
 	const isMarketplaceThemeSubscribed = useSelector(
 		( state ) =>
 			site &&
@@ -392,7 +408,16 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 	const isPluginBundleEligible = useIsPluginBundleEligible();
 	const isBundled = selectedDesign?.software_sets && selectedDesign.software_sets.length > 0;
 
+	const selectedDesignTier =
+		useSelector(
+			( state ) =>
+				selectedDesignThemeId && getThemeTierForTheme( state, selectedDesignThemeId )?.slug
+		) ?? FREE_THEME;
+
 	const isLockedTheme =
+		( isEnabled( 'themes/tiers' ) &&
+			selectedDesignTier === PERSONAL_THEME &&
+			! didPurchaseSelectedTheme ) ||
 		( selectedDesign?.is_premium && ! isPremiumThemeAvailable && ! didPurchaseSelectedTheme ) ||
 		( selectedDesign?.is_externally_managed &&
 			( ! isMarketplaceThemeSubscribed || ! isExternallyManagedThemeAvailable ) ) ||
@@ -456,6 +481,8 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 			plan = 'business-bundle';
 		} else if ( selectedDesign?.is_externally_managed ) {
 			plan = ! isExternallyManagedThemeAvailable ? PLAN_BUSINESS_MONTHLY : '';
+		} else if ( isEnabled( 'themes/tiers' ) && selectedDesignTier === PERSONAL_THEME ) {
+			plan = PLAN_PERSONAL;
 		} else {
 			plan = 'premium';
 		}

commit 182a463f3d6b81b59844ec41fe10339ce5ccab1e

Theme Tiers: Fix style variations layout in Design Picker (#86600)
Fixes a regression introduced in https://github.com/Automattic/wp-calypso/pull/86407 that caused the paid style variations to be incorrectly grouped with the default free style variation.

The culprit was that `didPurchaseSelectedTheme` started to return true for Free themes, although the theme has never been purchased technically, causing the logic below to believe that the theme was a purchased premium theme and thus shouldn't split the style variations.

client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
@@ -378,18 +378,17 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 			( product ) => product.product_slug === marketplaceProductSlug
 		) || marketplaceThemeProducts[ 0 ];
 
-	const didPurchaseSelectedTheme = useSelector( ( state ) => {
-		if ( ! site || ! selectedDesignThemeId ) {
-			return false;
-		}
+	const didPurchaseSelectedTheme = useSelector( ( state ) =>
+		site && selectedDesignThemeId
+			? isThemePurchased( state, selectedDesignThemeId, site.ID )
+			: false
+	);
 
-		if ( isEnabled( 'themes/tiers' ) ) {
-			return isThemeAllowedOnSite( state, site.ID, selectedDesignThemeId );
-		}
-
-		// @TODO Remove this once we have the new theme tiers live.
-		return isThemePurchased( state, selectedDesignThemeId, site.ID );
-	} );
+	const canSiteActivateTheme = useSelector( ( state ) =>
+		site && selectedDesignThemeId
+			? isThemeAllowedOnSite( state, site.ID, selectedDesignThemeId )
+			: false
+	);
 
 	const isMarketplaceThemeSubscribed = useSelector(
 		( state ) =>
@@ -417,7 +416,7 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 	const isLockedTheme =
 		( isEnabled( 'themes/tiers' ) &&
 			selectedDesignTier === PERSONAL_THEME &&
-			! didPurchaseSelectedTheme ) ||
+			! canSiteActivateTheme ) ||
 		( selectedDesign?.is_premium && ! isPremiumThemeAvailable && ! didPurchaseSelectedTheme ) ||
 		( selectedDesign?.is_externally_managed &&
 			( ! isMarketplaceThemeSubscribed || ! isExternallyManagedThemeAvailable ) ) ||