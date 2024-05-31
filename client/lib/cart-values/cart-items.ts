ForwardBuddy/wp-calypso

commit d8b4fee594dd26f2e984024d79831337c3a4a066

Domains: Create flow to buy a domain for Gravatar (#90414)
* Add `domain-for-gravatar` to reskinned flows array

* Add `domain-for-gravatar` flow in `flows-pure.js`

* Hide upsells in domain search page in `domain-for-gravatar` flow

* Skip `site-or-domain` step for the `domain-for-gravatar` flow

* Remove unnecessary steps from the flow

* Remove "Back to sites" backlink in Gravatar domain flow

* Show "Free domain for one year" in the Gravatar domain search

* Use "gravatar" vendor string for Gravatar domain search

* Add `domain_for_gravatar` property to domains selected in the Gravatar flow

* Small refactor

* Update copy with code review suggestions

* Remove unused variables

* Fix comments

client/lib/cart-values/cart-items.ts
@@ -47,6 +47,7 @@ import {
 	is100Year,
 	PLAN_FREE,
 } from '@automattic/calypso-products';
+import { isDomainForGravatarFlow } from '@automattic/onboarding';
 import { isWpComProductRenewal as isRenewal } from '@automattic/wpcom-checkout';
 import { getTld } from 'calypso/lib/domains';
 import { domainProductSlugs } from 'calypso/lib/domains/constants';
@@ -859,6 +860,10 @@ export function getDomainPriceRule(
 
 	if ( isMonthlyOrFreeFlow( flowName ) ) {
 		return 'PRICE';
+	}
+
+	if ( isDomainForGravatarFlow( flowName ) ) {
+		return 'FREE_FOR_FIRST_YEAR';
 	}
 
 	if ( domainAndPlanUpsellFlow ) {

client/components/domains/domain-product-price/index.jsx
@@ -137,6 +137,26 @@ class DomainProductPrice extends Component {
 		);
 	}
 
+	// This method returns "Free for the first year" text (different from "Free with plan")
+	renderFreeForFirstYear() {
+		const { translate } = this.props;
+
+		const className = classnames( 'domain-product-price', 'is-free-domain', {
+			'domain-product-price__domain-step-signup-flow': this.props.showStrikedOutPrice,
+		} );
+
+		return (
+			<div className={ className }>
+				<div className="domain-product-price__free-text">
+					<span className="domain-product-price__free-price">
+						{ translate( 'Free for the first year' ) }
+					</span>
+				</div>
+				{ this.renderReskinDomainPrice() }
+			</div>
+		);
+	}
+
 	renderFreeWithPlan() {
 		const className = classnames( 'domain-product-price', 'is-free-domain', {
 			'domain-product-price__domain-step-signup-flow': this.props.showStrikedOutPrice,
@@ -261,6 +281,8 @@ class DomainProductPrice extends Component {
 		switch ( this.props.rule ) {
 			case 'FREE_DOMAIN':
 				return this.renderFree();
+			case 'FREE_FOR_FIRST_YEAR':
+				return this.renderFreeForFirstYear();
 			case 'FREE_WITH_PLAN':
 			case 'INCLUDED_IN_HIGHER_PLAN':
 			case 'UPGRADE_TO_HIGHER_PLAN_TO_BUY':

client/signup/config/flows-pure.js
@@ -408,6 +408,16 @@ export function generateFlows( {
 			hideProgressIndicator: true,
 		},
 		{
+			name: 'domain-for-gravatar',
+			steps: [ 'domain-only', 'site-or-domain', 'site-picker' ],
+			destination: getDomainSignupFlowDestination,
+			description: 'Checkout flow for domains on Gravatar',
+			disallowResume: true,
+			lastModified: '2024-05-07',
+			showRecaptcha: true,
+			hideProgressIndicator: true,
+		},
+		{
 			name: 'site-selected',
 			steps: [ 'plans-site-selected-legacy' ],
 			destination: getSignupDestination,

client/signup/config/flows.js
@@ -22,19 +22,23 @@ function getCheckoutUrl( dependencies, localeSlug, flowName ) {
 		checkoutURL += `/${ localeSlug }`;
 	}
 
+	let checkoutBackUrl = `https://${ config( 'hostname' ) }/start/${ flowName }/domain-only`;
+	if ( config( 'env' ) !== 'production' ) {
+		const protocol = config( 'protocol' ) ?? 'https';
+		const port = config( 'port' ) ? ':' + config( 'port' ) : '';
+		const hostName = config( 'hostname' );
+
+		checkoutBackUrl = `${ protocol }://${ hostName }${ port }/start/${ flowName }/domain-only`;
+	}
+
 	return addQueryArgs(
 		{
 			signup: 1,
 			ref: getQueryArgs()?.ref,
 			...( dependencies.coupon && { coupon: dependencies.coupon } ),
-			...( [ 'domain' ].includes( flowName ) && {
+			...( [ 'domain', 'domain-for-gravatar' ].includes( flowName ) && {
 				isDomainOnly: 1,
-				checkoutBackUrl:
-					config( 'env' ) === 'production'
-						? `https://${ config( 'hostname' ) }/start/domain/domain-only`
-						: `${ config( 'protocol' ) ? config( 'protocol' ) : 'https' }://${ config(
-								'hostname'
-						  ) }${ config( 'port' ) ? ':' + config( 'port' ) : '' }/start/domain/domain-only`,
+				checkoutBackUrl,
 			} ),
 		},
 		checkoutURL

client/signup/steps/domains/index.jsx
@@ -457,6 +457,35 @@ export class RenderDomainsStep extends Component {
 
 		this.props.setDesignType( this.getDesignType() );
 
+		// For the `domain-for-gravatar` flow, add an extra `is_gravatar_domain` property to the domain registration product,
+		// pre-select the "domain" choice in the "site or domain" step and skip the others, going straight to checkout
+		if ( this.props.flowName === 'domain-for-gravatar' ) {
+			const domainForGravatarItem = domainRegistration( {
+				domain: suggestion.domain_name,
+				productSlug: suggestion.product_slug,
+				extra: {
+					is_gravatar_domain: true,
+				},
+			} );
+
+			this.props.submitSignupStep(
+				{
+					stepName: 'site-or-domain',
+					domainItem: domainForGravatarItem,
+					designType: 'domain',
+					siteSlug: domainForGravatarItem.meta,
+					siteUrl,
+					isPurchasingItem: true,
+				},
+				{ designType: 'domain', domainItem: domainForGravatarItem, siteUrl }
+			);
+			this.props.submitSignupStep(
+				{ stepName: 'site-picker', wasSkipped: true },
+				{ themeSlugWithRepo: 'pub/twentysixteen' }
+			);
+			return;
+		}
+
 		if ( migrateSite ) {
 			this.props.goToNextStep( 'site-migration' );
 		} else {
@@ -587,12 +616,13 @@ export class RenderDomainsStep extends Component {
 	}
 
 	shouldHideDomainExplainer = () => {
-		return this.props.flowName === 'domain';
+		const { flowName } = this.props;
+		return [ 'domain', 'domain-for-gravatar' ].includes( flowName );
 	};
 
 	shouldHideUseYourDomain = () => {
 		const { flowName } = this.props;
-		return [ 'domain' ].includes( flowName );
+		return [ 'domain', 'domain-for-gravatar' ].includes( flowName );
 	};
 
 	shouldDisplayDomainOnlyExplainer = () => {
@@ -1398,6 +1428,9 @@ export class RenderDomainsStep extends Component {
 			backUrl = domainManagementRoot();
 			backLabelText = translate( 'Back to All Domains' );
 		} else if ( ! previousStepBackUrl && 'domain-transfer' === flowName ) {
+			backUrl = null;
+			backLabelText = null;
+		} else if ( 'domain-for-gravatar' === flowName ) {
 			backUrl = null;
 			backLabelText = null;
 		} else if ( 'with-plugin' === flowName ) {

packages/domain-picker/src/utils/index.ts
@@ -4,6 +4,8 @@ import {
 	LINK_IN_BIO_TLD_FLOW,
 	ECOMMERCE_FLOW,
 	WOOEXPRESS_FLOW,
+	DOMAIN_FOR_GRAVATAR_FLOW,
+	isDomainForGravatarFlow,
 } from '@automattic/onboarding';
 import type { DomainSuggestions } from '@automattic/data-stores';
 
@@ -45,7 +47,8 @@ interface DomainSuggestionsVendorOptions {
 		| typeof LINK_IN_BIO_FLOW
 		| typeof LINK_IN_BIO_TLD_FLOW
 		| typeof ECOMMERCE_FLOW
-		| typeof WOOEXPRESS_FLOW;
+		| typeof WOOEXPRESS_FLOW
+		| typeof DOMAIN_FOR_GRAVATAR_FLOW;
 }
 type DomainSuggestionsVendor =
 	| 'variation2_front'
@@ -54,11 +57,15 @@ type DomainSuggestionsVendor =
 	| 'link-in-bio'
 	| 'link-in-bio-tld'
 	| 'newsletter'
-	| 'ecommerce';
+	| 'ecommerce'
+	| 'gravatar';
 
 export function getDomainSuggestionsVendor(
 	options: DomainSuggestionsVendorOptions = {}
 ): DomainSuggestionsVendor {
+	if ( isDomainForGravatarFlow( options.flowName ) ) {
+		return 'gravatar';
+	}
 	if ( options.flowName === LINK_IN_BIO_FLOW ) {
 		return 'link-in-bio';
 	}

packages/onboarding/src/utils/flows.ts
@@ -42,6 +42,7 @@ export const DOMAIN_TRANSFER = 'domain-transfer';
 export const GOOGLE_TRANSFER = 'google-transfer';
 export const HUNDRED_YEAR_PLAN_FLOW = 'hundred-year-plan';
 export const REBLOGGING_FLOW = 'reblogging';
+export const DOMAIN_FOR_GRAVATAR_FLOW = 'domain-for-gravatar';
 
 export const isLinkInBioFlow = ( flowName: string | null | undefined ) => {
 	return Boolean(
@@ -210,4 +211,8 @@ export const isVideoPressTVFlow = ( flowName: string | null | undefined ) => {
 	return Boolean(
 		flowName && [ VIDEOPRESS_TV_FLOW, VIDEOPRESS_TV_PURCHASE_FLOW ].includes( flowName )
 	);
+};
+
+export const isDomainForGravatarFlow = ( flowName: string | null | undefined ) => {
+	return Boolean( flowName && [ DOMAIN_FOR_GRAVATAR_FLOW ].includes( flowName ) );
 };

commit 15c7c83dc10cc8018f47f2f89f1c350ba77cf8f4

Gravatar Domains: Show the correct price formatting
This fixes an issue where the Gravatar flow introduced in PR #90414 showed discounted prices even when the discount was not applied.

client/lib/cart-values/cart-items.ts
@@ -839,6 +839,7 @@ export function getDomainPriceRule(
 		product_slug?: string;
 		productSlug?: string;
 		cost?: string;
+		sale_cost?: number;
 		domain_name?: string;
 		is_premium?: boolean;
 	},
@@ -863,7 +864,7 @@ export function getDomainPriceRule(
 	}
 
 	if ( isDomainForGravatarFlow( flowName ) ) {
-		return 'FREE_FOR_FIRST_YEAR';
+		return suggestion.sale_cost === 0 ? 'FREE_FOR_FIRST_YEAR' : 'PRICE';
 	}
 
 	if ( domainAndPlanUpsellFlow ) {